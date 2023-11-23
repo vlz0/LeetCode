c98b6b64aa5a50bbdaf66a5eab45f1ed4734451d
Daniel Velez <85912969+vlz0@users.noreply.github.com>
2023-11-23 01:05:01 -0500

    0303. Range Sum Query - Immutable
    jm quien dijo consultado

---
+++ 1. Easy/0303. Range Sum Query - Immutable.py
@@ -0,0 +1,12 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        arr = [nums[0]]
+        for i in range(1,len(nums)):
+            arr.append(arr[-1]+nums[i])
+        self.arr = arr
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.arr[right]
+        return self.arr[right] - self.arr[left - 1]
