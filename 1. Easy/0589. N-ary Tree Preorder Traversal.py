b072047478ee1edf6c9a4af2a1521be3cf494109
Daniel Velez <85912969+vlz0@users.noreply.github.com>
2024-02-25 01:19:23 -0500

    0589. N-ary Tree Preorder Traversal
    melo

---
+++ 1. Easy/0589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        arr = []
+        def order(root):
+            if root is None: return None
+            arr.append(root.val)
+            for i in root.children:
+                order(i)    
+        order(root)
+        return arr
