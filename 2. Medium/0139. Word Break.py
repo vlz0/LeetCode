239eb68eab51997d558e4588101281df442da6b3
Daniel Velez <85912969+vlz0@users.noreply.github.com>
2024-03-23 16:44:05 -0500

    0139. Word Break
    melo

---
+++ 2. Medium/0139. Word Break.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        max_len = max(map(len, wordDict))
+
+        for i in range(1, n + 1):
+            for j in range(i - 1, max(i - max_len - 1, -1), -1):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+
+        return dp[n]
